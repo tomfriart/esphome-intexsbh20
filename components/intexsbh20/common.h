999997b7a97f20b06c48040b65d4a1a053efeec2
tomfriart <106149027+tomfriart@users.noreply.github.com>
2026-03-20 17:12:41 +0100

    Refactor common.h for better readability and structure

--- components/intexsbh20/common.h
+++ components/intexsbh20/common.h
@@ -23,6 +23,74 @@
  * SPDX-License-Identifier: Apache-2.0
  *
  */
+#ifndef COMMON_H
+#define COMMON_H
+#include <limits.h>
+//#define SERIAL_DEBUG
+
+// Languages
+enum class LANG
+{
+  CODE = 0, EN = 1, DE = 2
+};
+
+// serial debugging
+#ifdef SERIAL_DEBUG
+#define DEBUG_MSG(...) Serial.printf( __VA_ARGS__ )
+#else
+#define DEBUG_MSG(...)
+#endif
+
+// time delta with overflow support
+static unsigned long timeDiff(unsigned long newTime, unsigned long oldTime)
+{
+  if (newTime >= oldTime)
+  {
+    return newTime - oldTime;
+  }
+  else
+  {
+    return ULONG_MAX - oldTime + newTime + 1;
+  }
+}
+
+// unsigned int delta with overflow support
+static unsigned long diff(unsigned int newVal, unsigned int oldVal)
+{
+  if (newVal >= oldVal)
+  {
+    return newVal - oldVal;
+  }
+  else
+  {
+    return UINT_MAX - oldVal + newVal + 1;
+  }
+}
+#endif /* COMMON_H *//*
+ * project:  Intex PureSpa SB-H20 WiFi Controller
+ *
+ * file:     common.h
+ *
+ * encoding: UTF-8
+ * created:  13th March 2021
+ *
+ * Copyright (C) 2021 Jens B.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
 
 #ifndef COMMON_H
 #define COMMON_H
