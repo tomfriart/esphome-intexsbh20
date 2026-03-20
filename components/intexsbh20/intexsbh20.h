06f5f3b78a5b6247560b8efd56597cd053f68817
tomfriart <106149027+tomfriart@users.noreply.github.com>
2026-03-20 17:54:16 +0100

    Update intexsbh20.h

--- components/intexsbh20/intexsbh20.h
+++ components/intexsbh20/intexsbh20.h
@@ -2,6 +2,57 @@
 #define INTEXSBH20_H_
 
 #include "esphome/core/component.h"
+#include "esphome/core/hal.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/sensor/sensor.h"
+#include "SBH20IO.h"
+
+namespace esphome {
+namespace sbh20 {
+
+class SBHClimate;
+class SBHSwitch;
+
+class IntexSBH20 : public PollingComponent
+{
+public:
+  void setup() override;
+  void loop() override;
+  void update() override;
+
+  SBH20IO* sbh() { return &sbh_; }
+
+  void set_climate(SBHClimate* climate) { climate_ = climate; }
+  void set_switch_power(SBHSwitch* sw) { switch_power_ = sw; }
+  void set_switch_filter(SBHSwitch* sw) { switch_filter_ = sw; }
+  void set_switch_bubble(SBHSwitch* sw) { switch_bubble_ = sw; }
+  void set_error_text_sensor(esphome::text_sensor::TextSensor* error) { error_text_ = error; }
+  void set_water_temperature_sensor(esphome::sensor::Sensor* temperature) { water_temperature_ = temperature; }
+
+  void set_data_pin(GPIOPin *pin) { data_pin_ = pin; }
+  void set_clock_pin(GPIOPin *pin) { clock_pin_ = pin; }
+  void set_enable_pin(GPIOPin *pin) { enable_pin_ = pin; }
+
+protected:
+  GPIOPin *data_pin_{nullptr};
+  GPIOPin *clock_pin_{nullptr};
+  GPIOPin *enable_pin_{nullptr};
+
+private:
+  SBH20IO sbh_;
+  SBHClimate* climate_ = nullptr;
+  SBHSwitch* switch_power_ = nullptr;
+  SBHSwitch* switch_filter_ = nullptr;
+  SBHSwitch* switch_bubble_ = nullptr;
+  esphome::text_sensor::TextSensor* error_text_ = nullptr;
+  esphome::sensor::Sensor* water_temperature_ = nullptr;
+};
+
+}}
+#endif#ifndef INTEXSBH20_H_
+#define INTEXSBH20_H_
+
+#include "esphome/core/component.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/components/sensor/sensor.h"
 #include "SBH20IO.h"
